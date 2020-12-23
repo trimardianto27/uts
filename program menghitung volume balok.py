93b74699a769a99204ff5010aa222a2902889de2
trimardianto27 <74308784+trimardianto27@users.noreply.github.com>
2020-12-23 21:27:14 +0700

    Add files via upload

---
+++ program menghitung volume balok.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"PROGRAM PHYTON MENGHITUNG VOLUME BALOK"
+print(pesan)
+first_name = 'tri'
+last_name = 'Mardianto'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+p = float(input("Panjang="))
+l = float(input("Lebar="))
+t = float(input("Tinggi="))
+
+v = p*l*t
+
+print ("Volume Balok=%0.2f"%v)
+
+
+# In[ ]:
+
+
+
+
